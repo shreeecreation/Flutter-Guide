c109f48f7ae88634c87ca188bfe224bb0ebacc2e
shreeecreation <shreeecreation720#gmail.com>
2023-10-28 20:23:44 +0545

    project intiialize

---
+++ android/app/src/main/kotlin/com/example/flutterguide/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterguide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
+
+import 'src/app/splash/presentation/pages/splash_page.dart';
+import 'src/core/helpers/adaptive_helper.dart';
+import 'src/core/widgets/adaptive_sizer_widget.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  AdaptiveHelper();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveSizer(builder: (context) {
+      return GetMaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const SplashPage(),
+      );
+    });
+  }
+}
